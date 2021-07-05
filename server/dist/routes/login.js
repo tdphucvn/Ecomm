f78d943962328833007a0b9c6262d686e3e341eb
Philip Tran <tran.philip02@gmail.com>
2021-07-06 00:37:15 +0200

    authentication routes

--- server/dist/routes/login.js
+++ server/dist/routes/login.js
@@ -1,1 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+var express_1 = __importDefault(require("express"));
+var authentication_1 = require("../controllers/authentication");
+var router = express_1.default.Router();
+exports.router = router;
+router.post('/login', authentication_1.loginRequest);
+router.post('/register', authentication_1.registerRequest);
+router.delete('/logout', authentication_1.logoutRequest);
---
+++ server/src/controllers/authentication.ts
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { IUser } from '../types/user';
+import User from '../model/User';
+import { Request, Response } from 'express';
+
+const saltRounds: number = 10;
+
+export const loginRequest = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const credentials = req.body as Pick<IUser, "username" | "password" >;
+        const user = await User.findOne({username: credentials.username});
+        if(user == null) throw new Error("User not found");
+        const validPassword = await bcrypt.compare(credentials.password, user.password);
+        if(!validPassword) throw new Error("Invalid Password");
+        
+        assigningTokens(user, res);
+
+        res.json({message: 'Succesfully logged in'});
+    } catch (error) {
+        res.json({message: error.message});
+    };
+};
+
+export const registerRequest = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const credentials = req.body as Pick<IUser, "username" | "email" | "password">; // Receiving the data from the user and checking if it is valid
+        const usernameExist = await User.findOne({username: credentials.username}); //Checking if the username already exists
+        if(usernameExist) throw new Error("Username is already used");
+        const emailExists = await User.findOne({email: credentials.email}); //Checking if the email is already being used
+        if(emailExists) throw new Error("Email is already used");
+        const salt: string = await bcrypt.genSalt(saltRounds); //Generating the salt for hashing
+        const hashedPassword: string = await bcrypt.hash(credentials.password, salt) //Hashing the password
+        const user: IUser = new User({ //Creating the user
+            username: credentials.username,
+            email: credentials.email,
+            password: hashedPassword
+        });
+        await user.save(); //Saving the new user
+
+        assigningTokens(user, res);
+
+        res.json({message: 'Thank your for signing up'});
+    } catch (error) {
+        res.json({message: error.message});
+    };
+};
+
+export const logoutRequest = async (req: Request, res: Response): Promise<void> => {
+    res.clearCookie('authorization'); 
+    res.clearCookie('refreshToken');
+    res.clearCookie('userSession');
+};
+
+const assigningTokens = (user: IUser, response: Response) => {
+    const accessSecretToken: string = `${process.env.ACCESS_TOKEN_SECRET}`;
+    const refreshSecretToken: string = `${process.env.REFRESH_TOKEN_SECRET}`;
+    const newAccessToken: string = jwt.sign({ user }, accessSecretToken, {expiresIn: '30min'}); //Creating an access token with JWT
+    const newRefreshToken: string = jwt.sign({ user }, refreshSecretToken, {expiresIn: '1day'}); //Creating a refresh token with JWT
+    
+    response.cookie('authorization', newAccessToken, {httpOnly: true}); // Using cookie to store Access token
+    response.cookie('refreshToken', newRefreshToken, {httpOnly: true}); // Using cookie to store Refresh token
+    response.cookie('userSession', user, {httpOnly: true}); // Using cookie to store data about the user
+}
